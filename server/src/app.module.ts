95f6d5c631da1e65c7f02904b6a1d8798f78d129
Dmitry <dima28rus97@gmail.com>
2019-05-13 21:38:12 +1000

    graphQL

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/graphql.ts
@@ -0,0 +1,10 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+export abstract class IQuery {
+    abstract temp__(): boolean | Promise<boolean>;
+}
