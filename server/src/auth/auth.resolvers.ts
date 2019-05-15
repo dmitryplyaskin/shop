224240f35b0c55b9641ad15536eb05ff9ee558e5
Dmitry <dima28rus97@gmail.com>
2019-05-15 22:00:38 +1000

    up

---
+++ server/src/auth/auth.resolvers.ts
@@ -0,0 +1,21 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+
+
+@Resolver('Auth')
+export class AuthResolver {
+  constructor(
+    private readonly authorsService: AuthorsService,
+    private readonly postsService: PostsService,
+  ) {}
+
+  @Query()
+  async author(@Args('id') id: number) {
+    return await this.authorsService.findOneById(id);
+  }
+
+  @ResolveProperty()
+  async posts(@Parent() author) {
+    const { id } = author;
+    return await this.postsService.findAll({ authorId: id });
+  }
+}
